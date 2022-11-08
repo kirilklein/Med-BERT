ddb88343a909fa1cdd9747df06f14acaf824f89a
Kiril Klein <kleinkiril94@gmail.com>
2022-11-08 12:32:40 +0100

    config file

--- medbert/models/MLM_pretraining.py
+++ medbert/models/MLM_pretraining.py
@@ -1,8 +1,9 @@
-from transformers import BertForMaskedLM
+from transformers import BertForMaskedLM, BertConfig
 from medbert.dataloader.MLM import MLMLoader
 from . import utils
 import torch
 import typer
+import json
 
 
 def main(data_file : str = typer.Argument(..., help="Tokenized data"),
@@ -11,10 +12,16 @@ def main(data_file : str = typer.Argument(..., help="Tokenized data"),
     epochs : int = typer.Argument(...),
     batch_size : int = typer.Option(16),
     max_len : int = typer.Option(512, help="maximum number of tokens in seq"),
+    config_file : str = typer.Option("configs\\mlm_config.json"),
     ):
-    model = BertForMaskedLM.from_pretrained('bert-base-uncased')
+    with open(config_file, 'r') as f:
+        config_dic = json.load(f)
     data = torch.load(data_file)
     vocab = torch.load(vocab_file)
+    #model = BertForMaskedLM.from_pretrained('bert-base-uncased')
+    config = BertConfig(vocab_size=len(vocab), **config_dic)
+    model = BertForMaskedLM(config)
+    assert False
     dataset = MLMLoader(data, vocab, max_len)
     loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True)
     trainer = utils.CustomMLMTrainer(dataset, model, epochs, batch_size, save_dir)
