d7c4416c1f220c05559d2b6457980752eae4e073
Kiril Klein <kikl@di.ku.dk>
2023-01-02 11:37:46 +0100

    updated masking

--- medbert/features/utils.py
+++ medbert/features/utils.py
@@ -1,23 +1,29 @@
-import random
+
 import numpy as np
 
 
-#TODO dont mask cls and sep tokens
-def random_mask(codes, vocab):
-    """One code is masked per patient"""
-    masked_codes = codes
-    labels = len(codes) * [-100] # -100 is ignored by loss function
-    mask_code = np.random.randint(len(codes))
-    prob = np.random.uniform()    
-    if prob < 0.8:
-        masked_codes[mask_code] = vocab['MASK']
-        labels[mask_code] = vocab['UNK']
-    # 10% randomly change token to random token
-    elif prob < 0.9:
-        masked_codes[mask_code] = random.choice(list(vocab.values())[3:]) # first three tokens reserved!
-    return masked_codes, labels
+def random_mask(idxs, vocab, mask_prob=0.15,
+    special_tokens=['MASK', 'UNK', 'PAD'], seed=0):
+    """mask code with 15% probability, 80% of the time replace with [MASK], 
+        10% of the time replace with random token, 10% of the time keep original"""
+    rng = np.random.default_rng(seed)
+    masked_idxs = idxs.copy()
+    special_idxs = [vocab[token] for token in special_tokens]
+    labels = len(idxs) * [-100] # -100 is ignored by loss function
+    
+    for i, idx in enumerate(idxs):
+        if idx in special_idxs:
+            continue
+        prob = rng.uniform()
+        if prob<mask_prob:
+            prob = rng.uniform()  
+            # 80% of the time replace with [MASK] 
+            if prob < 0.8:
+                masked_idxs[i] = vocab['MASK']
+            # 10% change token to random token
+            elif prob < 0.9:
+                masked_idxs[i] = rng.choice(list(vocab.values())[len(special_idxs):]) # first tokens are special!
+            # 10% keep original
+            labels[i] = idx
+    return masked_idxs, labels
 
-def seq_padding(seq, max_len, vocab):
-    return seq + (max_len-len(seq)) * [vocab['PAD']]
-
-#TODO torch.utils.data.random_split
