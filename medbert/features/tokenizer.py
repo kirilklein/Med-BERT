3c540f12ee4754e8bc1459bf2b816578e883b4e5
Kiril Klein <kikl@di.ku.dk>
2023-04-30 15:57:12 +0200

    Fix tokenizer, add padding and attention mask

--- medbert/features/tokenizer.py
+++ medbert/features/tokenizer.py
@@ -2,51 +2,82 @@ import torch
 import typer
 import pickle as pkl
 from os.path import join, split
+from tqdm import tqdm
 
 class EHRTokenizer():
-    def __init__(self, vocabulary=None):
+    def __init__(self, vocabulary=None, config=None):
+        self.frozen = False
+        self.config = config
         if isinstance(vocabulary, type(None)):
             self.vocabulary = {
-                'PAD':0,
-                'MASK':1,
-                'UNK':2,
+                '[PAD]':0,
+                '[MASK]':1,
+                '[UNK]':2,
             }
+            if config.sep_tokens:
+                self.vocabulary['[SEP]'] = len(self.vocabulary)
+            if config.cls_token:
+                self.vocabulary['[CLS]'] = len(self.vocabulary)
         else:
             self.vocabulary = vocabulary
 
-    def __call__(self, seq):
-        return self.batch_encode(seq)
+    def __call__(self, features):
+        return self.batch_encode(features)
 
     def encode(self, seq):
         for code in seq:
             if code not in self.vocabulary:
-                self.vocabulary[code] = len(self.vocabulary)
-        return [self.vocabulary[code] for code in seq]
+                if not self.frozen:
+                    self.vocabulary[code] = len(self.vocabulary)
+                
+        return [self.vocabulary.get(code, self.vocabulary['[UNK]']) for code in seq]
 
-    def batch_encode(self, seqs, max_len=None):
-        if isinstance(max_len, type(None)):
-            max_len = max([len(seq) for seq in seqs])
-        pat_ids = [seq[0] for seq in seqs]
-        los_seqs = [seq[1] for seq in seqs]
-        code_seqs = [seq[2] for seq in seqs] # icd codes
-        visit_seqs = [seq[3] for seq in seqs]
-        if isinstance(max_len, type(None)):
-            max_len = max([len(seq) for seq in code_seqs])    
-        output_code_seqs = []
-        output_visit_seqs = []
-        # let's do the padding later
-        for code_seq, visit_seq in zip(code_seqs, visit_seqs):
+    def batch_encode(self, features):
+        tokenized_data_dic = {'pats': [], 'los': [], 'concepts': [], 'segments': [], 'attention_mask': []}
+        concepts = [seq[2] for seq in features] # icd codes
+        pad_len = max([len(concept_seq) for concept_seq in concepts])   
+        if pad_len > self.config.truncation:
+            pad_len = self.config.truncation
+        target_seqs = []
+        out_visit_seqs = []
+        for patient in tqdm(features, desc="Tokenizing", total=len(concepts)):
             # truncation
-            if len(code_seq)>max_len:
-                code_seq = code_seq[:max_len]
-                visit_seq = visit_seq[:max_len]
+            concept_seq = patient[2]
+            visit_seq = patient[3]
+            if len(concept_seq)>self.config.truncation:
+                concept_seq = concept_seq[:self.config.truncation]
+                visit_seq = visit_seq[:self.config.truncation]
+            
             # Tokenizing
-            tokenized_code_seq = self.encode(code_seq)
-            output_code_seqs.append(tokenized_code_seq)
-            output_visit_seqs.append(visit_seq)
-        tokenized_data_dic = {'pats':pat_ids, 'los':los_seqs, 'codes':output_code_seqs, 
-                            'segments':output_visit_seqs}
+            tokenized_concept_seq = self.encode(concept_seq)
+            attention_mask = [1] * len(concept_seq)
+            # Pad tokenized code seq
+            if self.config.padding:
+                tokenized_concept_seq = self.pad(tokenized_concept_seq, pad_len)
+                visit_seq = self.pad(visit_seq, pad_len, pad_token=0)
+                attention_mask = self.pad(attention_mask, pad_len, pad_token=0)
+
+            target_seqs.append(tokenized_concept_seq)
+            out_visit_seqs.append(visit_seq)
+            tokenized_data_dic['concepts'].append(target_seqs) 
+            tokenized_data_dic['segments'].append(out_visit_seqs)
+            tokenized_data_dic['attention_mask'].append(attention_mask)
+
+        tokenized_data_dic['pats'] = [seq[0] for seq in features]
+        tokenized_data_dic['los'] = [seq[1] for seq in features]
         return tokenized_data_dic
+    
+    def pad(self, seq: dict,  pad_len: int, pad_token: int=None):
+        if pad_token is None:
+            pad_token = self.vocabulary['[PAD]']
+        difference = pad_len - len(seq)
+        if difference > 0:
+            return seq + [pad_token] * difference
+        else:
+            return seq
+
+    def freeze_vocab(self):
+        self.frozen = True
 
     def save_vocab(self, dest):
         print(f"Writing vocab to {dest}")
