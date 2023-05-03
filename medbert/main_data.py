0d6e8d763cbdf2d3ba0ad069b9d0432b07ad13b5
Kiril Klein <kikl@di.ku.dk>
2023-05-03 20:06:01 +0200

    Clean data processing pipeline

--- medbert/main_data.py
+++ medbert/main_data.py
@@ -23,27 +23,20 @@ def main():
     features = torch.load('../data/features/synthetic.pt') # list of lists
     train, val, test = Splitter(ratios=dataset_config.split_ratios)(features)
     # Tokenize
-    print(tokenizer_config)
     tokenizer = EHRTokenizer(config=tokenizer_config)
     encoded_train = tokenizer(train)
     tokenizer.freeze_vocab()
     tokenizer.save_vocab(join(dataset_config.working_dir, 'vocabulary.pt'))
     encoded_test = tokenizer(test)
     encoded_val = tokenizer(val)
-    patient = {key: values[3] for key, values in encoded_train.items()}
-    print(patient)
-    print({key: len(values) for key, values in patient.items()})
 
     # To dataset
     train_dataset = MLM_PLOS_Dataset(encoded_train, vocabulary=tokenizer.vocabulary, **dataset_config)
-    # test_dataset = MLMDataset(encoded_test, vocabulary=tokenizer.vocabulary)
-    # val_dataset = MLMDataset(encoded_val, vocabulary=tokenizer.vocabulary)
+    test_dataset = MLM_PLOS_Dataset(encoded_test, vocabulary=tokenizer.vocabulary, **dataset_config)
+    val_dataset = MLM_PLOS_Dataset(encoded_val, vocabulary=tokenizer.vocabulary, **dataset_config)
     torch.save(train_dataset, join(data_dir, 'dataset.train'))
-    patient = train_dataset.__getitem__(2)
-    print({key: len(values) for key, values in patient.items()})
-    print(patient)
-    # torch.save(test_dataset, join(data_dir, 'dataset.test'))
-    # torch.save(val_dataset,  join(data_dir, 'dataset.val'))
+    torch.save(test_dataset, join(data_dir, 'dataset.test'))
+    torch.save(val_dataset,  join(data_dir, 'dataset.val'))
     
     OmegaConf.save(config=tokenizer_config, f=join(dataset_config.working_dir, 'tokenizer.yaml'))
     OmegaConf.save(config=dataset_config, f=join(dataset_config.working_dir, 'dataset.yaml'))
