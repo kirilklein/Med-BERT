999ac7a3b7c5451b9085bdc4143ac8166395ac57
Kiril Klein <kikl@di.ku.dk>
2023-05-24 11:10:34 +0200

    Add ConceptLoader

--- medbert/data/utils.py
+++ medbert/data/utils.py
@@ -1,5 +1,47 @@
-import torch
+import glob
+import os
 from os.path import join
+
+import pandas as pd
+import torch
+
+
+class ConceptLoader():
+    def __call__(self, concepts=['diagnose', 'medication'], data_dir: str = 'formatted_data', patients_info: str = 'patients_info.csv'):
+        return self.load(concepts=concepts, data_dir=data_dir, patients_info=patients_info)
+    
+    def load(self, concepts=['diagnose', 'medication'], data_dir: str = 'formatted_data', patients_info: str = 'patients_info.csv'):
+        # Get all concept files
+        concept_paths = os.path.join(data_dir, 'concept.*')
+        path = glob.glob(concept_paths)
+
+        # Filter out concepts files
+        path = [p for p in path if p.split('.')[1] in concepts]
+        
+        # Load concepts
+        concepts = pd.concat([self._read_file(p) for p in path], ignore_index=True).drop_duplicates()
+        
+        concepts = concepts.sort_values('TIMESTAMP')
+
+        # Load patient data
+        patient_path = os.path.join(data_dir, patients_info)
+        patients_info = self._read_file(patient_path)
+
+        return concepts, patients_info
+
+    def _read_file(self, file_path: str) -> pd.DataFrame:
+        file_type = file_path.split(".")[-1]
+        if file_type == 'csv':
+            df = pd.read_csv(file_path)
+        elif file_type == 'parquet':
+            df = pd.read_parquet(file_path)
+
+        for col in self.detect_date_columns(df):
+            df[col] = df[col].apply(lambda x: x[:10] if isinstance(x, str) else x)
+            df[col] = pd.to_datetime(df[col], errors='coerce')
+            df[col] = df[col].dt.tz_localize(None)
+
+        return df
 
 class Splitter():
     def __init__(self, ratios: dict = {'train':0.7, 'val':0.2, 'test':0.1}) -> None:
