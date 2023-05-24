6826c5f1719211331798a22184db1fde9dc0dbeb
Kiril Klein <kikl@di.ku.dk>
2023-05-24 12:12:50 +0200

    Add Featuremakers and Creators

--- medbert/data/utils.py
+++ medbert/data/utils.py
@@ -6,6 +6,7 @@ from os.path import join, split
 import dateutil
 import pandas as pd
 import torch
+from creators import BaseCreator
 
 
 class ConceptLoader():
@@ -58,6 +59,69 @@ class ConceptLoader():
                 except:
                     continue
         return date_columns
+
+class FeatureMaker():
+    def __init__(self, config):
+        self.config = config
+
+        self.features = {
+            'concept': [],
+        }
+
+        self.order = {
+            'concept': 0,
+            'background': -1
+        }
+        self.creators = {creator.id: creator for creator in BaseCreator.__subclasses__()}
+        self.pipeline = self.create_pipeline()
+
+    def __call__(self, concepts: pd.DataFrame, patients_info: pd.DataFrame):
+        for creator in self.pipeline:
+            concepts = creator(concepts, patients_info)
+
+        features = self.create_features(concepts, patients_info)
+
+        return features
+    
+    def create_pipeline(self):
+        # Pipeline creation
+        pipeline = []
+        for id in self.config.features:
+            creator = self.creators[id](self.config)
+            pipeline.append(creator)
+            if getattr(creator, 'feature', None) is not None:
+                self.features[creator.feature] = []
+
+        # Reordering
+        pipeline_creators = [creator.feature for creator in pipeline if hasattr(creator, 'feature')]
+        for feature, pos in self.order.items():
+            if feature in pipeline_creators:
+                creator = pipeline.pop(pipeline_creators.index(feature))
+                pipeline.insert(pos, creator)
+
+        return pipeline
+
+    def create_features(self, concepts: pd.DataFrame, patients_info: pd.DataFrame) -> tuple:
+        # Add standard info
+        for pid, patient in concepts.groupby('PID'):
+            for feature, value in self.features.items():
+                value.append(patient[feature.upper()].tolist())
+
+        # Add outcomes if in config
+        outcomes = {outcome: [] for outcome in self.config.outcomes}
+        info_dict = patients_info.set_index('PID').to_dict('index')
+        origin_point = datetime(**self.config.features.abspos)
+        
+        # Add outcomes
+        for pid, patient in concepts.groupby('PID'):
+            for outcome in self.config.outcomes:
+                patient_outcome = info_dict[pid][f'OUTCOME_{outcome}']
+                if pd.isna(patient_outcome):
+                    outcomes[outcome].append(patient_outcome)
+                else:
+                    outcomes[outcome].append((patient_outcome - origin_point).total_seconds() / 60 / 60)
+
+        return self.features, outcomes
 
 class Splitter():
     def __init__(self, ratios: dict = {'train':0.7, 'val':0.2, 'test':0.1}) -> None:
