e704f30226472c6de68ed86756fb58e3ec5eea05
Kiril Klein <kikl@di.ku.dk>
2023-05-24 13:45:36 +0200

    Add Concepts
    Now concepts (df) are handled instead of processed features

--- medbert/main_data_pretrain.py
+++ medbert/main_data_pretrain.py
@@ -1,6 +1,6 @@
 from hydra import initialize, compose
 from omegaconf import OmegaConf
-from data.utils import Splitter
+from data.utils import Splitter, ConceptLoader, FeatureMaker, Excluder
 from features.tokenizer import EHRTokenizer
 from features.dataset import MLM_PLOS_Dataset
 import torch
@@ -8,41 +8,53 @@ from os.path import join
 
 def main():
     with initialize(config_path='../configs'):
-        tokenizer_config = compose(config_name='tokenizer.yaml')
-        dataset_config = compose(config_name='dataset.yaml')
+        cfg = compose(config_name='data_pretrain.yaml')
     
     # save configs in dataset_config.working_dir!
     
     
     """
+        Loads data
+        Finds outcomes
+        Censors
+        Creates features
+        Excludes patients with <k concepts
+        Splits data
         Tokenize
         To dataset
     """
 
-    # Overwrite nans and incorrect values
-    features = torch.load(dataset_config.in_path) # list of lists
-    splitter = Splitter(ratios=dataset_config.split_ratios)
+    print("Loading concepts...")
+    concepts, patients_info = ConceptLoader()(**cfg.loader)
+   
+    print("Creating feature sequences")
+    features = FeatureMaker(cfg)(concepts, patients_info)
+    
+    print("Exclude patients with <k concepts")
+    features = Excluder()(features, k=cfg.min_concepts)
+
+    print("Splitting data")
+    splitter = Splitter(ratios=cfg.split_ratios)
     splits = splitter(features)
-    splitter.save(dataset_config.out_dir)
+    splitter.save(cfg.out_dir)
     train, test, val = splits['train'], splits['test'], splits['val']
 
     # Tokenize
-    tokenizer = EHRTokenizer(config=tokenizer_config)
+    tokenizer = EHRTokenizer(config=cfg.tokenizer)
     train_encoded = tokenizer(train)
     tokenizer.freeze_vocabulary()
-    tokenizer.save_vocab(join(dataset_config.out_dir, 'vocabulary.pt'))
+    tokenizer.save_vocab(join(cfg.out_dir, 'vocabulary.pt'))
     test_encoded = tokenizer(test)
     val_encoded = tokenizer(val)
     # To dataset
-    train_dataset = MLM_PLOS_Dataset(train_encoded, vocabulary=tokenizer.vocabulary, **dataset_config)
-    test_dataset = MLM_PLOS_Dataset(test_encoded, vocabulary=tokenizer.vocabulary, **dataset_config)
-    val_dataset = MLM_PLOS_Dataset(val_encoded, vocabulary=tokenizer.vocabulary, **dataset_config)
-    torch.save(train_dataset, join(dataset_config.out_dir, 'dataset.train'))
-    torch.save(test_dataset, join(dataset_config.out_dir, 'dataset.test'))
-    torch.save(val_dataset,  join(dataset_config.out_dir, 'dataset.val'))
+    train_dataset = MLM_PLOS_Dataset(train_encoded, vocabulary=tokenizer.vocabulary, **cfg.dataset)
+    test_dataset = MLM_PLOS_Dataset(test_encoded, vocabulary=tokenizer.vocabulary, **cfg.dataset)
+    val_dataset = MLM_PLOS_Dataset(val_encoded, vocabulary=tokenizer.vocabulary, **cfg.dataset)
+    torch.save(train_dataset, join(cfg.out_dir, 'dataset.train'))
+    torch.save(test_dataset, join(cfg.out_dir, 'dataset.test'))
+    torch.save(val_dataset,  join(cfg.out_dir, 'dataset.val'))
     
-    OmegaConf.save(config=tokenizer_config, f=join(dataset_config.out_dir, 'tokenizer.yaml'))
-    OmegaConf.save(config=dataset_config, f=join(dataset_config.out_dir, 'dataset.yaml'))
+    OmegaConf.save(config=cfg, f=join(cfg.out_dir, 'data.yaml'))
     
 
 
