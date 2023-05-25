171e079a9694b6e576cb302e254df31c99a85aac
Kiril Klein <kikl@di.ku.dk>
2023-05-25 18:35:25 +0200

    Sort imports

--- medbert/main_perturb.py
+++ medbert/main_perturb.py
@@ -1,30 +1,30 @@
+from os.path import split
+
 import torch
-from hydra import initialize, compose
+from hydra import compose, initialize
 from hydra.utils import instantiate
 from torch.optim import AdamW
-
 from trainer.perturb import EHRPerturb
-from transformers import BertForSequenceClassification
-from transformers import BertConfig
-from os.path import split
+from transformers import BertConfig, BertForSequenceClassification
+
 
 def main():
     with initialize(config_path='../configs'):
         cfg: dict = compose(config_name='perturb.yaml')
 
+    print('Loading datasets...')
     train_dataset = torch.load(cfg.get('train_dataset', 'dataset.train'))
     val_dataset = torch.load(cfg.get('val_dataset', 'dataset.val'))
 
-    print('Loading BERT model...')
+    print(f'Loading finetuned BERT model from {cfg.model_path}')
     model_dir = split(cfg.model_path)[0]
-    
-    # Load the config from file
     config = BertConfig.from_pretrained(model_dir) 
     model = BertForSequenceClassification(config)
     model.load_state_dict(torch.load(cfg.model_path)['model_state_dict'], strict=False)
 
     model.eval() # we don't want to train the model, just use it to get the embeddings
 
+    print('Setting up optimizer...')
     opt = cfg.get('optimizer', {})
     optimizer = AdamW(
         model.parameters(),
@@ -33,8 +33,9 @@ def main():
         eps=opt.get('epsilon', 1e-8),
     )
 
+    print('Starting training...')
     trainer = EHRPerturb( 
-        model=model, 
+        bert_model=model, 
         optimizer=optimizer,
         train_dataset=train_dataset, 
         val_dataset=val_dataset, 
